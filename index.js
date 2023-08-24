39ef6bf6db279513542badc79db58596f82f52e8
RenandRosa <142436623+RenandRosa@users.noreply.github.com>
2023-08-24 16:59:39 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+// this is a js file
+// Renan da Rosa
+
+//A 
+//1. 
+//* a. false
+//* b. false
+//* c. true
+//* d. boolean
+
+//2.Está aparecendo no console os dois números juntos, ao invés de fazer a soma do primeiro número com o segundo número
+
+//3.
+/*
+let primeiroNumero = Number(prompt("Digite um numero!"));
+let segundoNumero = Number(prompt("Digite outro numero!"));
+const soma = primeiroNumero + segundoNumero;
+console.log(soma);
+*/
+
+//B
+//1.
+/*
+let idade = prompt("Qual a sua idade?");
+let idadeAmigo = prompt("Qual a idade do seu melhor amigo?");
+let idadeMaior = false;
+let diferençaIdade = 2
+console.log("Sua idade é maior do que a idade de seu melhor amigo?", idadeMaior, "Qual a diferença de idade?", diferençaIdade);
+*/
+
+//2.
+/*
+let númeroPar = Number(prompt("Insira um número par"));
+
+console.log(númeroPar % 2);
+//Todos aparecem 0 como resultado
+//Todos aparecem 1 como resultado
+*/
+
+//3.
+/*
+let idadeAnos = Number(prompt("Sua idade em anos:"));
+console.log(idadeAnos);
+
+let idadeMeses = idadeAnos * 12;
+console.log(idadeMeses);
+
+let idadeDias = idadeAnos * 365;
+console.log(idadeDias);
+
+let idadeHoras = idadeDias * 24;
+console.log(idadeHoras);
+*/
+
+//4.
+/*
+let númeroUm = Number(prompt("Digite um número:"));
+console.log(númeroUm);
+
+let númeroDois = Number(prompt("Digite um outro número"));
+console.log(númeroDois);
+
+númeroUm > númeroDois
+númeroUm === númeroDois
+númeroUm / númeroDois
+
+
+console.log(númeroUm > númeroDois);
+console.log(númeroUm === númeroDois);
+console.log(Boolean(númeroUm / númeroDois));
+console.log(Boolean(númeroDois / númeroUm));
+*/
